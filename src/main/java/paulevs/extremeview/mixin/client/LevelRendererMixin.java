0ff14e554a089339348513f1474eef2fce95cafe
paulevsGitch <paulevs@yandex.ru>
2024-03-17 10:16:48 +0300

    Removed usage of mixinextras

--- src/main/java/paulevs/extremeview/mixin/client/LevelRendererMixin.java
+++ src/main/java/paulevs/extremeview/mixin/client/LevelRendererMixin.java
@@ -1,16 +1,14 @@
 package paulevs.extremeview.mixin.client;
 
-import com.llamalad7.mixinextras.sugar.Local;
-import com.llamalad7.mixinextras.sugar.ref.LocalIntRef;
 import net.minecraft.client.render.LevelRenderer;
-import net.minecraft.entity.living.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.At.Shift;
+import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import paulevs.extremeview.ExtremeView;
 
 @Mixin(LevelRenderer.class)
@@ -29,13 +27,11 @@ public class LevelRendererMixin {
 		this.sectionCounZ = this.sectionCounX;
 	}
 	
-	@Inject(method = "updateOcclusion(Lnet/minecraft/entity/living/LivingEntity;ID)I", at = @At(
-		value = "INVOKE",
-		target = "Lnet/minecraft/client/render/LevelRenderer;updateOcclusion(II)V",
-		shift = Shift.BEFORE,
-		ordinal = 0
-	))
-	private void extremeview_updateOcclusion(LivingEntity entity, int d, double par3, CallbackInfoReturnable<Integer> info, @Local(index = 19) LocalIntRef stride) {
-		if (this.sectionCounX == 1) stride.set(8);
+	@ModifyConstant(
+		method = "updateOcclusion(Lnet/minecraft/entity/living/LivingEntity;ID)I",
+		constant = @Constant(intValue = 16, ordinal = 0)
+	)
+	private int extremeview_changeSectionCount1(int sectionCount) {
+		return this.sectionCounX == 1 ? 8 : sectionCount;
 	}
 }
